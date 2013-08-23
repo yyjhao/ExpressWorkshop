6d68098cdafafb1d1df969394b99168f221ac04b
yyjhao <yyjhao@gmail.com>
2013-08-23 22:15:42 +0800

    initial upload

---
+++ blog/blogPost.js
@@ -0,0 +1,12 @@
+module.exports = function(db){
+    var mongoose = require("mongoose");
+    mongoose.connect("mongodb://" + db.host + "/" + db.database);
+
+    var schema = new mongoose.Schema({
+        title: String,
+        body: String,
+        date: { type: Date, default: Date.now }
+    });
+
+    return mongoose.model("BlogPost", schema);
+};
---
+++ blog/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    domain: "localhost",
+    port: 3000,
+    secret: "thegame",
+    db: {
+        host: "localhost",
+        database: "MyBlog"
+    }
+};
